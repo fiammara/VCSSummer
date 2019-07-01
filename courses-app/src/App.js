ac2a6f88c3927af1a18ffa1d4fcc07fdea7c54ab
zakevicius <m.zakevicius@gmail.com>
2019-07-01 09:01:25 +0300

    Code indentation

--- courses-app/src/App.js
+++ courses-app/src/App.js
@@ -8,56 +8,56 @@ import CourseCards from "./Models/CourseCard";
 import InfoAboutCourse from "./InfoAboutCourse";
 
 const App = () => (
-    <BrowserRouter>
+  <BrowserRouter>
     {/* Search start */}
-        <div><Search/></div>
+    <div><Search /></div>
     {/* Search end */}
-        <div>
+    <div>
 
-        <div className="NavContainer">
-            <button className="NavButton">
-                <NavLink to="/home">Home</NavLink>
-            </button>
+      <div className="NavContainer">
+        <button className="NavButton">
+          <NavLink to="/home">Home</NavLink>
+        </button>
 
-            <button className="NavButton">
-                <NavLink to={{ pathname: "/courses" }}>Link to courses</NavLink>
-            </button>
+        <button className="NavButton">
+          <NavLink to={{ pathname: "/courses" }}>Link to courses</NavLink>
+        </button>
 
-                <button className="NavButton">
-                    <NavLink to={{ pathname: "/courseCards" }}>Link to courses</NavLink>
-                </button>
+        <button className="NavButton">
+          <NavLink to={{ pathname: "/courseCards" }}>Link to courses</NavLink>
+        </button>
 
-                <button className="NavButton">
-                    <NavLink to={{ pathname: "/infoAboutCourse" }}>Info about course</NavLink>
-                </button>
+        <button className="NavButton">
+          <NavLink to={{ pathname: "/infoAboutCourse" }}>Info about course</NavLink>
+        </button>
 
-            </div>
-            <Switch>
-                <Route exact path="/courses" component={Courses} />
-                <Route exact path="/home" component={Home} />
-                <Route exact path="/courseCards" component={CourseCards.CourseCards} />
-                <Route exact path="/infoAboutCourse" component={InfoAboutCourse} />
+      </div>
+      <Switch>
+        <Route exact path="/courses" component={Courses} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/courseCards" component={CourseCards.CourseCards} />
+        <Route exact path="/infoAboutCourse" component={InfoAboutCourse} />
 
-            </Switch>
-        </div>
-    </BrowserRouter>
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
 
 //search+jumbotron
-const Search=()=>{
-    return(<div>
-        <div>
-        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1"/>
-        </div>
+const Search = () => {
+  return (<div>
+    <div>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" />
+    </div>
 
-        <div class="example">
-  <input type="text" placeholder="Search.." name="search"/>
-  <button type="submit">Search</button>
-        </div>
+    <div class="example">
+      <input type="text" placeholder="Search.." name="search" />
+      <button type="submit">Search</button>
+    </div>
 
-    </div>);
+  </div>);
 }
 
 //calendar
@@ -129,119 +129,119 @@ const Search=()=>{
 </div>);
 */
 //categoryList1
-const CategoryList1=()=>{
-  return(
-  
-  <div class="cart1">
-	<div class="imageC">Image</div>
-	<div class="titleC">Kurso pavadinimas</div>
-	<div class="ratingC">reitingas</div>
-	<div class="kainaC">kurso kaina</div>
-	<div class="infoC">Laikas,data,vieta</div>
-</div>);
+const CategoryList1 = () => {
+  return (
+
+    <div class="cart1">
+      <div class="imageC">Image</div>
+      <div class="titleC">Kurso pavadinimas</div>
+      <div class="ratingC">reitingas</div>
+      <div class="kainaC">kurso kaina</div>
+      <div class="infoC">Laikas,data,vieta</div>
+    </div>);
 }
 
 //categoryList2
-const CategoryList2=()=>{
-  return( 
-  <div>
-  <div class="cart1">
-	<div class="imageC">Image</div>
-	<div class="titleC">Kurso pavadinimas</div>
-	<div class="ratingC">reitingas</div>
-	<div class="kainaC">kurso kaina</div>
-	<div class="infoC">Laikas,data,vieta</div>
-</div>
-<div class="cart2">
-	<div class="infoDaugiauC">
-		<h3>Lygis</h3>
-		<h3>Lektorius</h3>
-		<h3>Kalendorius</h3>
-		<h3>Sertifikatas</h3>
-		<h3>Atsiliepimai</h3>
-		<h3>Top skilai</h3>
-	</div>
-</div>
-</div>);
+const CategoryList2 = () => {
+  return (
+    <div>
+      <div class="cart1">
+        <div class="imageC">Image</div>
+        <div class="titleC">Kurso pavadinimas</div>
+        <div class="ratingC">reitingas</div>
+        <div class="kainaC">kurso kaina</div>
+        <div class="infoC">Laikas,data,vieta</div>
+      </div>
+      <div class="cart2">
+        <div class="infoDaugiauC">
+          <h3>Lygis</h3>
+          <h3>Lektorius</h3>
+          <h3>Kalendorius</h3>
+          <h3>Sertifikatas</h3>
+          <h3>Atsiliepimai</h3>
+          <h3>Top skilai</h3>
+        </div>
+      </div>
+    </div>);
 }
 
-const SignInLogIn=()=>{
-  return(<div>
-  
- <h2>Tabs</h2>
-<p>Click on the buttons inside the tabbed menu:</p>
+const SignInLogIn = () => {
+  return (<div>
 
-<div class="tab">
-  <button class="tablinks" onclick="openCity(event, 'London')">Registracija</button>
-  <button class="tablinks" onclick="openCity(event, 'Paris')">Prisijungimas</button>
-</div>
+    <h2>Tabs</h2>
+    <p>Click on the buttons inside the tabbed menu:</p>
 
-  <form action="/action_page.php"/>
-  
+    <div class="tab">
+      <button class="tablinks" onclick="openCity(event, 'London')">Registracija</button>
+      <button class="tablinks" onclick="openCity(event, 'Paris')">Prisijungimas</button>
+    </div>
+
+    <form action="/action_page.php" />
+
     <h1>Register</h1>
     <p>Please fill in this form to create an account.</p>
-    <hr/>
+    <hr />
 
     <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required/>
+    <input type="text" placeholder="Enter Email" name="email" required />
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required/>
+    <input type="password" placeholder="Enter Password" name="psw" required />
 
     <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required />
     <hr></hr>
     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
     <button type="submit" class="registerbtn">Register</button>
- <form/>
-  <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
-  </div>
+    <form />
+    <div class="container signin">
+      <p>Already have an account? <a href="#">Sign in</a>.</p>
+    </div>
 
-  <h2>Responsive Social Login Form</h2>
+    <h2>Responsive Social Login Form</h2>
 
-<div class="container">
-  <form action="/action_page.php">
-    <div class="row">
-      <h2 style="text-align:center">Login with Social Media or Manually</h2>
-      <div class="vl">
-        <span class="vl-innertext">or</span>
-      </div>
+    <div class="container">
+      <form action="/action_page.php">
+        <div class="row">
+          <h2 style="text-align:center">Login with Social Media or Manually</h2>
+          <div class="vl">
+            <span class="vl-innertext">or</span>
+          </div>
 
-      <div class="col">
-        <a href="#" class="fb btn">
-          <i class="fa fa-facebook fa-fw"></i> Login with Facebook
+          <div class="col">
+            <a href="#" class="fb btn">
+              <i class="fa fa-facebook fa-fw"></i> Login with Facebook
          </a>
-        <a href="#" class="twitter btn">
-          <i class="fa fa-twitter fa-fw"></i> Login with Twitter
+            <a href="#" class="twitter btn">
+              <i class="fa fa-twitter fa-fw"></i> Login with Twitter
         </a>
-        <a href="#" class="google btn"><i class="fa fa-google fa-fw">
-          </i> Login with Google+
+            <a href="#" class="google btn"><i class="fa fa-google fa-fw">
+            </i> Login with Google+
         </a>
-      </div>
+          </div>
 
-      <div class="col">
-        <div class="hide-md-lg">
-          <p>Or sign in manually:</p>
+          <div class="col">
+            <div class="hide-md-lg">
+              <p>Or sign in manually:</p>
+            </div>
+
+            <input type="text" name="username" placeholder="Username" required />
+            <input type="password" name="password" placeholder="Password" required />
+            <input type="submit" value="Login" />
+          </div>
+
         </div>
+      </form>
+    </div>
 
-        <input type="text" name="username" placeholder="Username" required/>
-        <input type="password" name="password" placeholder="Password" required/>
-        <input type="submit" value="Login"/>
-      </div>
+    <div class="bottom-container">
+
+      <a href="#" style="color:white" class="btn">Sign up</a>
+      <a href="#" style="color:white" class="btn">Forgot password?</a>
 
     </div>
-  </form>
-</div>
+  </div>
 
-<div class="bottom-container">
-  
-      <a href="#" style="color:white" class="btn">Sign up</a>  
-      <a href="#" style="color:white" class="btn">Forgot password?</a>
-   
-  </div>
-  </div>
-  
-);
+  );
 }
