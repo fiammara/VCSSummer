9038e5c99b9fa21482cc80770031f3074595b8dd
rasa <fiammara@gmail.com>
2019-07-05 16:23:32 +0300

    link to a filter

--- courses-app/src/Main.js
+++ courses-app/src/Main.js
@@ -3,20 +3,15 @@ import React from 'react';
 class Main extends React.Component {
 
     render() {
+
         return (
             <div className="main">
 
                 <p>Main</p>
                 <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" />
-
+                 <button onClick={this.props.search} > DETALI PAIEŠKA</button> 
                 <div>
-                    <div className="NavContainer">
-
-                        <button >DETALI PAIEŠKA
-                            </button>
-
-                    </div>
-
+                
                 </div>
 
             </div>
