02e0d23a3f8e1d37adba2e3e96b41728bd073c44
aurimas12 <35044937+aurimas12@users.noreply.github.com>
2019-07-10 19:31:17 +0300

    Create Title.js
    Task nr.9 Title.js update

---
+++ courses-app/src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Main.css";
+
+class Title extends React.Component {
+
+    render() {
+
+        return (
+            <div className="title">
+                <h2 className="titleName">Lorem Ipsum</h2>
+            </div>
+        );
+    }
+}
+export default Title;
