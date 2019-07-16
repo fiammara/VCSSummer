4638d54aa1bad1d4010ab871e254fda4367bdb5f
rasa <fiammara@gmail.com>
2019-07-16 13:07:18 +0300

    duk page styling

--- courses-app/src/Duk.js
+++ courses-app/src/Duk.js
@@ -6,26 +6,29 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
 
 class Duk extends React.Component {
 
     render() {
-       
+
         return (
             <div>
-                <Typography >Užduokite klausimą?</Typography>
+                <Container>
+                <Typography align="left"> Užduokite klausimą?</Typography>
 
-                <TextField variant="outlined" margin="normal" />
+                <TextField variant="outlined" fullWidth margin="normal" />
 
-                <Typography >Dažniausiai užduodami klausimai</Typography>
+                <Typography align="left"> Dažniausiai užduodami klausimai</Typography>
+                <br />
+                <ExpansionPanel>
+                    <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2a-content"
+                        id="panel2a-header" >
+                        <Typography >Lorem ipsum dolor sit amet consetetur</Typography>
+                    </ExpansionPanelSummary>
 
-                <ExpansionPanel>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel2a-content"
-                        id="panel2a-header" >
-                        <Typography >Lorem ipsum dolor sit amet consetetur</Typography>
-                    </ExpansionPanelSummary>
                     <ExpansionPanelDetails>
                         <Typography>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
@@ -79,11 +82,13 @@ class Duk extends React.Component {
                     </ExpansionPanelDetails>
                 </ExpansionPanel>
                 <br />
-                <h6>
+                <Typography >
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </h6>
-                <Typography>Ar buvo naudinga?<Button variant="outlined" > Taip </Button>  <Button variant="outlined"> Ne </Button></Typography>
-               
+                    </Typography>
+                    <br />
+                <Typography align="left">Ar buvo naudinga? <span> </span> <Button variant="outlined" > Taip </Button>  <Button variant="outlined"> Ne </Button></Typography>
+                <br />
+                </Container>
             </div>)
     }
 }
