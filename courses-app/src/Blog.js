068d13f38a9763af3ef0448e7330693f198439f2
aurimas12 <35044937+aurimas12@users.noreply.github.com>
2019-07-17 17:56:43 +0300

    Update Blog.js
    Blog post update

--- courses-app/src/Blog.js
+++ courses-app/src/Blog.js
@@ -1,25 +1,125 @@
 import React from "react";
-//import InfoProgram from "./Models/Program";
-//import InfoLector from "./Models/Lector";
-//import InfoCalendar from "./Models/CourseCalendar";
-//import InfoCert from "./Models/Cert";
-//import InfoReviews from "./Models/Review";
-//import InfoContacts from "./Models/Contacts";
+import Title from "./Title";
+import "./Main.css";
+import "./Blog.css";
 
 const Blog = () => {
     return (
-        <div>
-            <h3>pasirinkti pagal kategorijas</h3>
-            <input type="text" name="fname" placeholder="First name"></input>
+        <div className="main">
+               
+            <div className="divas">
+                <Title />
+                <div className="textBlock">
+                    
+                    <h3 className="intro1">pasirinkti pagal kategorijas</h3>
+                       
+                    <input className="search1" type="text" name="fname" placeholder="  Visos"></input>
+                    <div className="blogPost">
+                        <div className="blog1">
+                            <div className="blogImg">
+                                <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlyqcEbtZy1vItK_jDT9iA-wqLmkj7nb8iYipzXcbMhAaovjhLOQ"} alt=""/>
+                            </div>
+                        </div>
+                        
+                        <div className="blog2">
+                            <div className="blogTitle">
+                                <h3 className="blogTitleH">Sed Ut perspiciatis Unde Omnis 
+                                    Iste Natus Error Sit Voluplatem</h3>
+                            </div>
+                            <div className="blogAuthorImg">
+                            <img src={"http://www.pixelprints.co.in/images/portfolio/graphic-design/logo/small/logo_3.png"} alt="" />
+                            
+                            </div>
+                            <div className="blogAuthor">
+                                <h3>Rob Robinson</h3>
+                                <h3>Title</h3>
+                            </div>
+                            <div className="blogDate">
+                                <h3>2019.5.25</h3>
+                            </div> 
+                        </div>
 
-            <div class="grid-container">
-                <div class="item1">baneris</div>
-                <div class="item2">imgPost</div>
-                <div class="item3">title</div>
-                <div class="item4">Author</div>
-                <div class="item5">Data</div>
-                <div class="item6">text post</div>
-                <div class="item7">Button</div>
+                        <div className="blog3">
+                            <div className="blogTextwBtn">
+                                <p>It is a long established fact that a reader will be distracted by the readable content 
+                                    of a page when looking at its layout. The point of
+                                </p>
+                                <button>Skaityti</button>
+                             </div>
+                        </div>
+                    </div>
+                    
+
+                    <div className="blogPost">
+                        <div className="blog1">
+                            <div className="blogImg">
+                                <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlyqcEbtZy1vItK_jDT9iA-wqLmkj7nb8iYipzXcbMhAaovjhLOQ"} alt=""/>
+                            </div>
+                        </div>
+                        
+                        <div className="blog2">
+                            <div className="blogTitle">
+                                <h3 className="blogTitleH">Sed Ut perspiciatis Unde Omnis 
+                                    Iste Natus Error Sit Voluplatem</h3>
+                            </div>
+                            <div className="blogAuthorImg">
+                            <img src={"http://www.pixelprints.co.in/images/portfolio/graphic-design/logo/small/logo_3.png"} alt="" />
+                            
+                            </div>
+                            <div className="blogAuthor">
+                                <h3>Rob Robinson</h3>
+                                <h3>Title</h3>
+                            </div>
+                            <div className="blogDate">
+                                <h3>2019.5.25</h3>
+                            </div> 
+                        </div>
+
+                        <div className="blog3">
+                            <div className="blogTextwBtn">
+                                <p>It is a long established fact that a reader will be distracted by the readable content 
+                                    of a page when looking at its layout. The point of
+                                </p>
+                                <button>Skaityti</button>
+                             </div>
+                        </div>
+                    </div>
+
+                    <div className="blogPost">
+                        <div className="blog1">
+                            <div className="blogImg">
+                                <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlyqcEbtZy1vItK_jDT9iA-wqLmkj7nb8iYipzXcbMhAaovjhLOQ"} alt=""/>
+                            </div>
+                        </div>
+                        
+                        <div className="blog2">
+                            <div className="blogTitle">
+                                <h3 className="blogTitleH">Sed Ut perspiciatis Unde Omnis 
+                                    Iste Natus Error Sit Voluplatem</h3>
+                            </div>
+                            <div className="blogAuthorImg">
+                            <img src={"http://www.pixelprints.co.in/images/portfolio/graphic-design/logo/small/logo_3.png"} alt="" />
+                            
+                            </div>
+                            <div className="blogAuthor">
+                                <h3>Rob Robinson</h3>
+                                <h3>Title</h3>
+                            </div>
+                            <div className="blogDate">
+                                <h3>2019.5.25</h3>
+                            </div> 
+                        </div>
+
+                        <div className="blog3">
+                            <div className="blogTextwBtn">
+                                <p>It is a long established fact that a reader will be distracted by the readable content 
+                                    of a page when looking at its layout. The point of
+                                </p>
+                                <button>Skaityti</button>
+                             </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
