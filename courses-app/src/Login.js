dc8b26f5279e71ddb182890b5403f203c4938332
zakevicius <m.zakevicius@gmail.com>
2019-07-20 13:02:03 +0300

    Login page with css

--- courses-app/src/Login.js
+++ courses-app/src/Login.js
@@ -1,43 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-export default function Login() {
+class Login extends Component {
 
   handleClick = (e) => {
     e.preventDefault();
   }
 
-  return (
-    <div className="flex wrap mar1">
-      <div className="flex input flex-center marY1">
-        <i className="far fa-envelope" />
-        FACEBOOK
-      </div>
-      <div className="flex input flex-center marY1">
-        <i className="far fa-envelope" />
-        GOOGLE
-      </div>
-      <form className="flex wrap">
+  render() {
+    return (
+      <div className="flex wrap mar1">
         <div className="flex input flex-center marY1">
           <i className="far fa-envelope" />
-          <input
-            type="email"
-            label="Įveskite e-mail"
-            className="auth-input"
-            placeholder="Įveskite email"
-          />
+          FACEBOOK
         </div>
         <div className="flex input flex-center marY1">
-          <i className="fas fa-lock" />
-          <input
-            type="password"
-            label="Įveskite slaptažodį"
-            className="auth-input input"
-            placeholder="Įveskite slaptažodį"
-          />
+          <i className="far fa-envelope" />
+          GOOGLE
         </div>
-        <button onClick={this.handleClick}className="flex input flex-center marY1">Mygtukas</button>
-      </form>
-    </div>
-  );
+        <form className="flex wrap">
+          <div className="flex input flex-center marY1">
+            <i className="far fa-envelope" />
+            <input
+              type="email"
+              label="Įveskite e-mail"
+              className="auth-input"
+              placeholder="Įveskite email"
+            />
+          </div>
+          <div className="flex input flex-center marY1">
+            <i className="fas fa-lock" />
+            <input
+              type="password"
+              label="Įveskite slaptažodį"
+              className="auth-input input"
+              placeholder="Įveskite slaptažodį"
+            />
+          </div>
+          <button onClick={this.handleClick} className="flex input flex-center marY1">Mygtukas</button>
+        </form>
+      </div>
+    );
+  }
 };
+
+export default Login;
