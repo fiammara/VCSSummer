6d0cd2b9e5fa7b626358f8df27fec5c821b95b97
rasa <fiammara@gmail.com>
2019-08-13 12:19:43 +0300

    props fixed

--- courses-app/src/ACourse.js
+++ courses-app/src/ACourse.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ACourse = props => 
-
- (
-                    
-   <div>{props.name}</div>);   
-
- ACourse.propTypes = {   
-      name: PropTypes.string.isRequired,      
+const ACourse = (props) => {
+const { name } = props;
+return (<div>{name}</div>);
 };
 
+ACourse.propTypes = {    
+  name: PropTypes.string.isRequired, 
+};
 export default ACourse;
--- courses-app/src/BlogPost.js
+++ courses-app/src/BlogPost.js
@@ -4,39 +4,47 @@ import './Blog.css';
 import PropTypes from 'prop-types';
 
 const BlogPost = (props) => {
+  const {
+ blogImg, 
+ blogTitle, 
+ blogAuthorImg, 
+ blogAuthor, 
+ title, 
+ blogDate, 
+ blogTextwBtn 
+} = props;
 
   return ( 
     <div className="blogPost">
 
       <div className="blog1">
         <div className="blogImg">
-          {props.blogImg}
+          {blogImg}
         </div>
       </div>
 
       <div className="blog2">
         <div className="blogTitle">
           <h3 className="blogTitleH">
-            {props.blogTitle}
+            {blogTitle}
           </h3>
         </div>
         <div className="blogAuthorImg">
-          {props.blogAuthorImg}
-
+          {blogAuthorImg}
         </div>
         <div className="blogAuthor">
-          <h3>{props.blogAuthor}</h3>
-          <h3>{props.title}</h3>
+          <h3>{blogAuthor}</h3>
+          <h3>{title}</h3>
         </div>
         <div className="blogDate">
-          <h3>{props.blogDate}</h3>
+          <h3>{blogDate}</h3>
         </div>
       </div>
 
       <div className="blog3">
         <div className="blogTextwBtn">
           <p>
-            {props.blogTextwBtn}
+            {blogTextwBtn}
           </p>
           <button type="button">Skaityti</button>
         </div>
@@ -52,8 +60,7 @@ const BlogPost = (props) => {
       blogAuthor: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       blogDate: PropTypes.string.isRequired,
-      blogTextwBtn: PropTypes.string.isRequired,
-        
+      blogTextwBtn: PropTypes.string.isRequired       
 };
 
 export default BlogPost; 
